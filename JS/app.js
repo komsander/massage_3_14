e43f373fbc1696fc51a1e328e6373e4360994bb2
ALEKSANDR FIZIK <109596308+komsander@users.noreply.github.com>
2024-03-01 18:21:11 +1000

    Add files via upload

---
+++ JS/app.js
@@ -0,0 +1,6 @@
+$("h1").hide(1).css({ visibility: "visible" }).fadeTo(1, 0.1).fadeTo(4000, 1);
+// $(".intro>h4")
+//   .hide(1)
+//   .css({ visibility: "visible" })
+//   .fadeTo(1, 0.1)
+//   .fadeTo(4000, 1);
